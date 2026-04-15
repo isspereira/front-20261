9d701be30bc9a83c3b7e2c62099539a26980f5c1
Isabele Pereira <isabelle.sousa973@gmail.com>
2026-04-14 21:15:57 -0300

    feat: conclui pratica04. Fecha #04

--- praticas/aluno-online-react/src/pages/Requerimentos.jsx
+++ praticas/aluno-online-react/src/pages/Requerimentos.jsx
@@ -1,33 +1,18 @@
-import Sidebar from "../components/Sidebar";
-import Header from "../components/Header";
 import "./Requerimentos.css";
-import "../components/Layout.css";
+import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
 
 function Requerimentos() {
+  const colunas = ["Tipo", "Status"];
+
+  const dados = [
+    { tipo: "Declaração", status: "Em análise" },
+  ];
+
   return (
-    <main className="layout">
-      <Sidebar />
-      <section className="main">
-        <Header />
-        <section className="content">
-          <h2>Requerimentos</h2>
-          <table>
-            <thead>
-              <tr>
-                <th>Tipo</th>
-                <th>Status</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Declaração</td>
-                <td>Em análise</td>
-              </tr>
-            </tbody>
-          </table>
-        </section>
-      </section>
-    </main>
+    <Layout titulo="Requerimentos" subtitulo="Solicitações">
+      <Tabela titulos={colunas} dados={dados} />
+    </Layout>
   );
 }
 
