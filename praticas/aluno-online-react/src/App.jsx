9f68e09fb733ce351852c7b1a6a7b7caf53abe82
Isabele Pereira <isabelle.sousa973@gmail.com>
2026-05-13 20:53:56 -0300

    feat: conclui pratica06. Fecha #06

--- praticas/aluno-online-react/src/App.jsx
+++ praticas/aluno-online-react/src/App.jsx
@@ -1,21 +1,25 @@
-import Dashboard from './pages/Dashboard';
-import Notas from './pages/Notas';
-import Faltas from './pages/Faltas';
-import Boletos from './pages/Boletos';
-import Requerimentos from './pages/Requerimentos';
+import { Routes, Route } from 'react-router-dom'
+
+import Layout from './components/Layout'
+
+import Dashboard from './pages/Dashboard'
+import Faltas from './pages/Faltas'
+import Notas from './pages/Notas'
+import Boletos from './pages/Boletos'
+import Requerimentos from './pages/Requerimentos'
 
 function App() {
-  const pagina = 4; 
-
   return (
-    <>
-      {pagina === 1 && <Dashboard />}
-      {pagina === 2 && <Notas />}
-      {pagina === 3 && <Faltas />}
-      {pagina === 4 && <Boletos />}
-      {pagina === 5 && <Requerimentos />}
-    </>
-  );
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="faltas" element={<Faltas />} />
+        <Route path="notas" element={<Notas />} />
+        <Route path="boletos" element={<Boletos />} />
+        <Route path="requerimentos" element={<Requerimentos />} />
+      </Route>
+    </Routes>
+  )
 }
 
-export default App;
+export default App
