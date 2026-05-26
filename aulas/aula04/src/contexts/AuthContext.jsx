2d54f25a119541862666d08107e93a82e2cfed08
Isabele Pereira <isabelle.sousa973@gmail.com>
2026-05-26 19:53:18 -0300

    adiciona codigo aula 4

--- aulas/aula04/src/contexts/AuthContext.jsx
+++ aulas/aula04/src/contexts/AuthContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useContext, useState } from "react";
 const AuthContext = createContext();
 
 function AuthProvider({ children}) {
-    const [logado, setLogado] = useState(false);
+    const [logado, setLogado] = useState(true);
     const [usuario, setUsuario] = useState({});
 
     const login = (dados) => {
