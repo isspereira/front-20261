0f84362d1997b1ec2ec91db7ceb3403c165ffa46
Isabele Pereira <isabelle.sousa973@gmail.com>
2026-06-09 21:04:21 -0300

    feat: conclui pratica09. Fecha #ID

---
+++ aulas/aula06/src/App.jsx
@@ -0,0 +1,16 @@
+import { Router, Route, Routes } from "react-router";
+import Listagem from "./pages/Listagem";
+import Formulario from "./pages/Formulario";
+
+function App() {
+  return (
+  <Routes>
+    <Route path="/produtos" element={<Listagem />} />
+    <Route path="/produtos/novo" element={<Formulario />} />
+    <Route path="/produtos/editar/:id" element={<Formulario />} />
+    </Routes>
+    );
+
+}
+
+export default App;
